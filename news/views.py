2ce12848854301a80c97a043eeb1255d5b078b23
Anastasiya <156897871+AnastasiyaTuhtarkina@users.noreply.github.com>
2024-06-02 17:01:15 +0300

    Add files via upload

--- news/views.py
+++ news/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import *
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = 'date_post'
+    context_object_name = 'posts'
+    template_name = 'news/templates/news/post_detail.html'
+
+
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'news/templates/news/post_detail.html'
+    #queryset = Post.objects.get(pk=pk)
