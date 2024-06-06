eb9395e4d722d57c99ced65a7f7d2b7051484ad4
Anastasiya Tuhtarkina <Nastya1909@mail.ru>
2024-06-06 20:39:30 +0300

    NewsPortal: добавление D4

---
+++ templates/news/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Post
+from django.core.exceptions import ValidationError
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+       model = Post
+       fields = [
+           'name_post',
+           'categories_post',
+           'text_post',
+       ]
+       widgets = {
+           'name_post': forms.Textarea(attrs={'class': 'form-taxt', 'cols': 70, 'rows': 3}),
+           'text_post': forms.Textarea(attrs={'class': 'form-taxt', 'cols': 70, 'rows': 10}),
+       }
+
+    def clean_name(self):
+        name = self.cleaned_data["name_post"]
+        if name[0].islower():
+            raise ValidationError(
+                "Название должно начинаться с заглавной буквы"
+            )
+        return name
+    
+    def clean_text(self):
+        name = self.cleaned_data["text_post"]
+        if name[0].islower():
+            raise ValidationError(
+                "Текст должен начинаться с заглавной буквы"
+            )
+        return name
---
+++ templates/news/templatetags/custom_filters.py
@@ -0,0 +1,19 @@
+from django import template
+
+
+register = template.Library()
+
+words = ['дурак', 'мудак', 'попа', 'титька', 'хрен', 'вонючка', 'чурбан', 'остолоп', 'чумазую', 'прожорливые', 'монар']
+
+
+# Регистрируем наш фильтр под именем currency, чтоб Django понимал,
+# что это именно фильтр для шаблонов, а не простая функция.
+@register.filter()
+def censor(value):
+    if type(value) == str:
+        value = value.lower().split()
+        for i, word in enumerate(value):
+            if word in words:
+                value[i] = word[0] + '*' * len(word[1:]) 
+        return ' '.join(value)
+
